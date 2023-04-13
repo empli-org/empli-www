3cfd941eeb81eb9324d6e16c3ffd5845116f8875
Aldo R. Robles <90198035+roblesdotdev@users.noreply.github.com>
2023-04-13 18:16:54 -0300

    client/initial config (#1)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/global.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
