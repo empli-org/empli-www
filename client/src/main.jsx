c3cf36cb1cda7a2efaf4441d24580802c3e7afd1
roblesdotdev <httpsnip@gmail.com>
2023-04-13 18:22:47 -0300

    setup prettier

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles/global.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles/global.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
