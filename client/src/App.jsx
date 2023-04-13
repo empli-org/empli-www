e78842b72001c1d84336dc65dda3690db8efadc0
roblesdotdev <httpsnip@gmail.com>
2023-04-13 18:08:52 -0300

    feat: setup tailwind css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div>
-      <h1>Working</h1>
+    <div className="flex min-h-screen items-center justify-center bg-slate-50 text-slate-900">
+      <h1 className="text-3xl font-bold">Working</h1>
     </div>
   );
 }
