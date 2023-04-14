9009f176d8fa2c086983e12bff2a0497f2c83be9
francisco alejandro myers muñoz <franciscomyers@gmail.com>
2023-04-14 00:10:26 -0400

    FEAT: New component Card conditional

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 export default function LandingPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-slate-50 text-slate-800">
