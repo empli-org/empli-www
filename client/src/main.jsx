11c3290604b3addfa4d74f158ce9f266c7c6915e
Ronald Javier Echeverry <69412980+ronaldjdev@users.noreply.github.com>
2023-04-14 05:22:56 -0500

    Chore: Add implementation of Redux Toolkit (#7)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './styles/global.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
+import { Provider } from 'react-redux'
+import { store } from './redux/store/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
