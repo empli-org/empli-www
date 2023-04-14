7d1bb2a43df7ebdf8844828f724455ce5384ae76
roblesdotdev <httpsnip@gmail.com>
2023-04-14 08:01:52 -0300

    feat: get endpoints

--- api/src/controllers/careers.ts
+++ api/src/controllers/careers.ts
@@ -2,7 +2,17 @@ import { Request, Response } from "express";
 import db from "../utils/db";
 
 export async function getAllCareers(req: Request, res: Response) {
-  const careers = await db.career.findMany();
+  try {
+    const careers = await db.career.findMany();
 
-  return res.json(careers);
+    return res.json({
+      status: 200,
+      error: false,
+      data: careers,
+    });
+  } catch {
+    return res
+      .status(500)
+      .json({ status: 500, error: true, message: `Fail to fetch data` });
+  }
 }
--- api/src/routes/talents.ts
+++ api/src/routes/talents.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { getAllTalents } from "../controllers/talents";
+import { getAllTalents, getTalentById } from "../controllers/talents";
 
 export function getTalentRoutes() {
   const router = express.Router();
 
   router.route("/").get(getAllTalents);
+
+  router.route("/:id").get(getTalentById);
 
   return router;
 }
