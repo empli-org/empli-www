c7775466ed29d88ca46901127080ddfb5ef675b8
Aldo R. Robles <90198035+roblesdotdev@users.noreply.github.com>
2023-04-14 19:10:50 -0300

    feat: api/redefine datamodel (#15)

--- api/src/controllers/talents.ts
+++ api/src/controllers/talents.ts
@@ -3,11 +3,56 @@ import db from "../utils/db";
 
 export async function getAllTalents(req: Request, res: Response) {
   try {
+    const { key } = req.query;
     const talents = await db.talent.findMany({
+      ...(key && {
+        where: {
+          OR: [
+            {
+              career: {
+                name: { contains: key as string, mode: "insensitive" },
+              },
+            },
+            {
+              skills: {
+                some: {
+                  name: { contains: key as string, mode: "insensitive" },
+                },
+              },
+            },
+          ],
+        },
+      }),
       include: {
-        location: true,
         career: true,
-        skills: true,
+        skills: {
+          select: {
+            name: true,
+          },
+        },
+        contactInfo: {
+          select: {
+            location: {
+              select: {
+                city: true,
+                country: true,
+              },
+            },
+          },
+        },
+        experienceInfo: {
+          select: {
+            projects: {
+              select: {
+                name: true,
+                description: true,
+                startDate: true,
+                endDate: true,
+                amount: true,
+              },
+            },
+          },
+        },
       },
     });
 
@@ -22,7 +67,18 @@ export async function getAllTalents(req: Request, res: Response) {
 export async function getTalentById(req: Request, res: Response) {
   try {
     const { id } = req.params;
-    const talent = await db.talent.findUnique({ where: { id } });
+    const talent = await db.talent.findUnique({
+      where: { id },
+      include: {
+        contactInfo: true,
+        experienceInfo: {
+          select: {
+            projects: true,
+            references: true,
+          },
+        },
+      },
+    });
     if (!talent) {
       return res.status(404).json({
         error: true,
