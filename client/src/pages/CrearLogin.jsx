457aa813adefc6bb9d366caa6124ebb83b095e74
Gisell24 <100234864+Gisell24@users.noreply.github.com>
2023-04-14 10:32:28 -0300

    listo navbar (#10)

---
+++ client/src/pages/CrearLogin.jsx
@@ -0,0 +1,9 @@
+const CrearLogin = () => {
+  return (
+    <div>
+      <h1>Hola soy Login</h1>
+    </div>
+  )
+}
+
+export default CrearLogin
---
+++ client/src/pages/Ofertas.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Ofertas = () => {
+  return <div></div>
+}
+
+export default Ofertas
