533a65b4d81375c1723baccfb37c4c544d252c77
Solideomyers <106446764+Solideomyers@users.noreply.github.com>
2023-04-15 18:58:42 +0200

    Update ErrorPage.jsx
    add PageNotFound

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -8,12 +8,7 @@ export default function ErrorPage() {
 
   return (
     <div id="error-page">
-      {/* <h1>Oops!</h1> */}
-      {/* <p>Sorry, an unexpected error has occurred.</p> */}
       <PageNOtFound />
-      {/* <p>
-        <i>{error.statusText || error.message}</i>
-      </p> */}
     </div>
   )
 }
