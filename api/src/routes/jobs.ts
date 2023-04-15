a036fa57d748810530e3e0381f9d9be6a1c23e3a
roblesdotdev <httpsnip@gmail.com>
2023-04-15 17:12:12 -0300

    fix: use search jobs endpoint

--- api/src/routes/jobs.ts
+++ api/src/routes/jobs.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { getAllJobs, getJobByCode } from "../controllers/jobs";
+import { getAllJobs, getJobByCode, searchJobsByKey } from "../controllers/jobs";
 
 export function getJobsRoutes() {
   const router = express.Router();
 
   router.route("/").get(getAllJobs);
+
+  router.route("/search").get(searchJobsByKey);
 
   router.route("/:code").get(getJobByCode);
 
