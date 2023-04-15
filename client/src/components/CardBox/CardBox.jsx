d1dd99d566002eeccc33f52849217eb1363fb46d
Solideomyers <106446764+Solideomyers@users.noreply.github.com>
2023-04-15 04:15:10 +0200

    CardProfile (#18)

--- client/src/components/CardBox/CardBox.jsx
+++ client/src/components/CardBox/CardBox.jsx
@@ -4,7 +4,7 @@ import { MiniCard } from '../MiniCard/MiniCard'
 
 //We can received data from props or bring from global state
 function CardBox({ data }) {
-  const gruposDeDiez = data.reduce((accumulator, obj, index) => {
+    const gruposDeDiez = data.reduce((accumulator, obj, index) => {
     const indexGroup = Math.floor(index / 10)
 
     if (!accumulator[indexGroup]) {
@@ -17,13 +17,13 @@ function CardBox({ data }) {
   }, [])
 
   return (
-    <div className="bg-gray-900 flex flex-col w-3/4 mx-auto m-5 p-10 rounded-3xl">
-      <div className="p-5 text-white flex flex-col items-end justify-end m-5">
-        <div className="font-bold text-2xl">
-          <h1>Top Profesionales</h1>
+    <div className='bg-gray-900 flex flex-col w-3/4 mx-auto m-5 p-10 rounded-3xl'>
+      <div className='p-5 text-white flex flex-col items-end justify-end m-5'>
+        <div className='font-bold text-2xl'>
+           <h1>Top Profesionales</h1>
         </div>
         <div>
-          <span>Mas de 200 ofertas por dia</span>
+            <span>Mas de 200 ofertas por dia</span>
         </div>
       </div>
       <div className="flex overflow-x-scroll p-5">
