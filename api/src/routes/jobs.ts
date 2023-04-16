2de510d4599047d11ba861eed8f3c7fff547d54a
roblesdotdev <httpsnip@gmail.com>
2023-04-15 22:44:47 -0300

    fix: use new controllers

--- api/src/routes/jobs.ts
+++ api/src/routes/jobs.ts
@@ -1,13 +1,19 @@
 import express from "express";
-import { getAllJobs, getJobByCode, searchJobsByKey } from "../controllers/jobs";
+import {
+  getAllJobs,
+  getJobAreas,
+  getJobByCode,
+  getJobLocations,
+  searchJobsByKey,
+} from "../controllers/jobs";
 
 export function getJobsRoutes() {
   const router = express.Router();
 
   router.route("/").get(getAllJobs);
-
   router.route("/search").get(searchJobsByKey);
-
+  router.route("/locations").get(getJobLocations);
+  router.route("/areas").get(getJobAreas);
   router.route("/:code").get(getJobByCode);
 
   return router;
