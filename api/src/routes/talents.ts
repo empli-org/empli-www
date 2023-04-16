aa4ac0ffe3b733bd2d3f89071256c0e493256559
roblesdotdev <httpsnip@gmail.com>
2023-04-16 17:25:42 -0300

    fix: call correct controller

--- api/src/routes/talents.ts
+++ api/src/routes/talents.ts
@@ -4,13 +4,13 @@ import {
   getAllTalents,
   getTalentById,
 } from "../controllers/talents";
-import { getAllCareers } from "../controllers/careers";
+import { getCareers } from "../controllers/talents";
 
 export function getTalentRoutes() {
   const router = express.Router();
 
   router.route("/").get(getAllTalents);
-  router.route("/careers").get(getAllCareers);
+  router.route("/careers").get(getCareers);
   router.route("/skills").get(getAllSkills);
   router.route("/:id").get(getTalentById);
 
