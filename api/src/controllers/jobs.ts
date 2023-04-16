b3ac750abd0cc77c988d5c9cfed8192a26ab321f
roblesdotdev <httpsnip@gmail.com>
2023-04-16 02:09:21 -0300

    feat: return count job items for pagination

--- api/src/controllers/jobs.ts
+++ api/src/controllers/jobs.ts
@@ -1,5 +1,6 @@
 import { Request, Response, query } from "express";
 import db from "../utils/db";
+import { Prisma } from "@prisma/client";
 
 export async function getJobAreas(req: Request, res: Response) {
   try {
@@ -70,23 +71,8 @@ export async function getAllJobs(req: Request, res: Response) {
     const { key, page, location, area } = req.query;
     const currentPage = Math.max(Number(page) || 1, 1);
     const peerPage = 10;
-    const jobs = await db.job.findMany({
-      where: {
-        ...(key && { title: { contains: key as string, mode: "insensitive" } }),
-        ...(location && {
-          location: {
-            OR: [
-              { city: { contains: location as string, mode: "insensitive" } },
-              {
-                country: { contains: location as string, mode: "insensitive" },
-              },
-            ],
-          },
-        }),
-        ...(area && {
-          area: { contains: area as string, mode: "insensitive" },
-        }),
-      },
+    const countOptions: Prisma.JobFindManyArgs = {};
+    const options: Prisma.JobFindManyArgs = {
       select: {
         code: true,
         title: true,
@@ -108,9 +94,32 @@ export async function getAllJobs(req: Request, res: Response) {
       },
       take: peerPage,
       skip: (currentPage - 1) * peerPage,
-    });
+    };
+    options.where = {
+      ...(key && { title: { contains: key as string, mode: "insensitive" } }),
+      ...(location && {
+        location: {
+          OR: [
+            { city: { contains: location as string, mode: "insensitive" } },
+            {
+              country: { contains: location as string, mode: "insensitive" },
+            },
+          ],
+        },
+      }),
+      ...(area && {
+        area: { contains: area as string, mode: "insensitive" },
+      }),
+    };
+    countOptions.where = options.where;
 
-    return res.json(jobs);
+    const jobs = await db.job.findMany(options);
+    const count = await db.job.findMany(countOptions);
+
+    return res.json({
+      data: jobs,
+      count,
+    });
   } catch {
     return res
       .status(500)
