8c9a6f4336fd9352a1ecc42936b39dccf5bf5bc4
Aldo R. Robles <90198035+roblesdotdev@users.noreply.github.com>
2023-04-16 17:01:00 -0300

    Api release (#30)

--- api/src/routes/talents.ts
+++ api/src/routes/talents.ts
@@ -1,11 +1,17 @@
 import express from "express";
-import { getAllTalents, getTalentById } from "../controllers/talents";
+import {
+  getAllSkills,
+  getAllTalents,
+  getTalentById,
+} from "../controllers/talents";
+import { getAllCareers } from "../controllers/careers";
 
 export function getTalentRoutes() {
   const router = express.Router();
 
   router.route("/").get(getAllTalents);
-
+  router.route("/careers").get(getAllCareers);
+  router.route("/skills").get(getAllSkills);
   router.route("/:id").get(getTalentById);
 
   return router;
