d8d523cc478ddf3110132080533835f3cccce7b1
Gisell24 <100234864+Gisell24@users.noreply.github.com>
2023-04-21 11:08:59 -0300

    listo El Home para empresas y las Notificaciones en el formulario de Contacto (#72)

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -18,6 +18,7 @@ import {
 import UserProfileForm from './pages/UserForm'
 import PricingPage from './pages/Info/Pricing'
 import Media from './pages/Community/Media'
+import HomeEmpresa from './pages/HomeEmpresa'
 import { AuthLayout } from './pages/Auth/Layout'
 import { Onboarding } from './pages/Account/Onboarding'
 import { Payment } from './pages/Account/Payment'
@@ -37,6 +38,10 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Landing />,
+      },
+      {
+        path: '/info/empresas',
+        element: <HomeEmpresa />,
       },
       {
         path: '/pricing',
