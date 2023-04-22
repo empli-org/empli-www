7def46411b18d3a1fb3dd6c339ec80e3dc4daf18
Aldo R. Robles <90198035+roblesdotdev@users.noreply.github.com>
2023-04-22 15:23:57 -0300

    fix navbar items (#82)

---
+++ client/src/pages/Info/InfoProfessionals.jsx
@@ -0,0 +1,9 @@
+import { Container } from '@/components'
+
+export default function InfoProfessionals() {
+  return (
+    <Container>
+      <h1>Info para profesionales</h1>
+    </Container>
+  )
+}
