cfbaff1dc8355dfd074f9d767c09b67ef372f5e1
roblesdotdev <httpsnip@gmail.com>
2023-04-22 15:22:53 -0300

    fix navbar items

--- client/src/components/Navbar/NavbarItems.jsx
+++ client/src/components/Navbar/NavbarItems.jsx
@@ -1,33 +1,18 @@
 import { Link } from 'react-router-dom'
 import { useMenuContext } from './MenuItem'
 
-const companies = [
+const contact = [
   {
-    name: 'Marketplace',
-    description: 'Encuentra los mejores profesionales en nuestra plataforma',
-    to: '/talents',
-    icon: GroupIcon,
+    name: 'Sobre nosotros',
+    description: 'Descubre lo que somos como nuestros servicios',
+    to: '/about',
+    icon: InfoCircle,
   },
   {
-    name: 'Panel',
-    description: 'Facilidad para administrar las búsquedas que necesitas',
-    to: '/login',
-    icon: ChartBarIcon,
-  },
-]
-
-const talents = [
-  {
-    name: 'Ofertas',
-    description: 'Encuentra las ofertas adecuadas para tu perfil',
-    to: '/offers',
-    icon: BellAlertIcon,
-  },
-  {
-    name: 'Verificación',
-    description: 'Mantenemos tu perfil actualizado y verificado',
-    to: '/offers',
-    icon: CheckBadgeIcon,
+    name: 'Contacto',
+    description: 'Encuentra la forma de mantenernos conectados',
+    to: '/contact',
+    icon: EnvelopeIcon,
   },
 ]
 
@@ -45,58 +30,6 @@ const community = [
     icon: PlayCircleIcon,
   },
 ]
-
-export function CompanyItems() {
-  const { close } = useMenuContext()
-  return (
-    <div className="grid grid-cols-1 gap-6">
-      {companies.map(item => (
-        <Link
-          onClick={() => close()}
-          key={item.name}
-          to={item.to}
-          className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
-        >
-          <div className="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
-            <span className="flex h-10 w-10 items-center justify-center rounded-lg bg-hint-of-red text-orange-500 sm:h-12 sm:w-12">
-              {item.icon ? <item.icon aria-hidden="true" /> : null}
-            </span>
-          </div>
-          <div className="ml-4">
-            <p className="text-sm font-medium text-gray-900">{item.name}</p>
-            <p className="text-sm text-gray-500">{item.description}</p>
-          </div>
-        </Link>
-      ))}
-    </div>
-  )
-}
-
-export function TalentItems() {
-  const { close } = useMenuContext()
-  return (
-    <div className="grid grid-cols-1 gap-6">
-      {talents.map(item => (
-        <Link
-          onClick={() => close()}
-          key={item.name}
-          to={item.to}
-          className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
-        >
-          <div className="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
-            <span className="flex h-10 w-10 items-center justify-center rounded-lg bg-sail/50 text-blue-whale sm:h-12 sm:w-12">
-              {item.icon ? <item.icon aria-hidden="true" /> : null}
-            </span>
-          </div>
-          <div className="ml-4">
-            <p className="text-sm font-medium text-gray-900">{item.name}</p>
-            <p className="text-sm text-gray-500">{item.description}</p>
-          </div>
-        </Link>
-      ))}
-    </div>
-  )
-}
 
 export function CommunityItems() {
   const { close } = useMenuContext()
@@ -124,79 +57,29 @@ export function CommunityItems() {
   )
 }
 
-function GroupIcon() {
+export function ContactItems() {
+  const { close } = useMenuContext()
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="h-5 w-5"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z"
-      />
-    </svg>
-  )
-}
-
-function ChartBarIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="h-5 w-5"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
-      />
-    </svg>
-  )
-}
-
-function BellAlertIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="h-5 w-5"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5"
-      />
-    </svg>
-  )
-}
-
-function CheckBadgeIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="h-5 w-5"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"
-      />
-    </svg>
+    <div className="grid grid-cols-1 gap-6">
+      {contact.map(item => (
+        <Link
+          onClick={() => close()}
+          key={item.name}
+          to={item.to}
+          className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+        >
+          <div className="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
+            <span className="flex h-10 w-10 items-center justify-center rounded-lg bg-fog/50 text-blue-whale sm:h-12 sm:w-12">
+              {item.icon ? <item.icon aria-hidden="true" /> : null}
+            </span>
+          </div>
+          <div className="ml-4">
+            <p className="text-sm font-medium text-gray-900">{item.name}</p>
+            <p className="text-sm text-gray-500">{item.description}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
   )
 }
 
@@ -238,6 +121,44 @@ function NewspaperIcon() {
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"
+      />
+    </svg>
+  )
+}
+
+function EnvelopeIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-5 w-5"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+      />
+    </svg>
+  )
+}
+
+function InfoCircle() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-5 w-5"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
       />
     </svg>
   )
