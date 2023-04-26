a78dde7bb6f1c225a521d94d98cfacdd40c9b49f
Santiago Jurado <104171442+santij25@users.noreply.github.com>
2023-04-25 21:33:35 -0300

    FEAT: Add alert and field cleaning (#97)

--- client/src/pages/Info/InfoProfessionals.jsx
+++ client/src/pages/Info/InfoProfessionals.jsx
@@ -14,43 +14,43 @@ export default function InfoProfessionals() {
   }, [])
   return (
     <div className="w-screen">
-      <div className="flex h-all bg-oficina bg-cover">
+      <div className="bg-oficina bg-cover h-all flex">
         <h1
-          className="flex w-full items-center justify-center font-amenable text-6xl font-bold text-hint-of-red"
+          className="font-amenable text-6xl text-hint-of-red font-bold flex justify-center items-center w-full"
           data-aos="fade-up"
         >
           Descubre nuevas oportunidades laborales
         </h1>
       </div>
-      <div className="flex w-full items-center justify-evenly bg-hint-of-red">
-        <div className="my-32 flex w-1/3 flex-col" data-aos="fade-right">
-          <h1 className="text-center font-amenable text-3xl font-bold text-blue-whale">
+      <div className="flex justify-evenly items-center w-full bg-hint-of-red">
+        <div className="flex flex-col w-1/3 my-32" data-aos="fade-right">
+          <h1 className="font-amenable text-3xl text-center font-bold text-blue-whale">
             ¿Te gustaría encontrar nuevas oportunidades laborales en la
             construcción?
           </h1>
           <div className="my-6 w-56 rounded-2xl border-t-4 border-bg-green"></div>
           <br />
-          <p className="font-quicksand-light text-2xl font-semibold text-blue-whale">
+          <p className="font-quicksand-light font-semibold text-2xl text-blue-whale">
             Si es así, ¡estás en el lugar correcto! Únete a nuestra comunidad de
             talentos y descubre nuevas oportunidades de trabajo, mejora tus
             habilidades laborales y amplía tu red de contactos en el sector de
             la construcción.
           </p>
         </div>
-        <img src={pj} alt="logo" className="h-80 w-all" data-aos="fade-right" />
+        <img src={pj} alt="logo" className="w-all h-80" data-aos="fade-right" />
       </div>
-      <div className="flex w-full items-center justify-evenly bg-hint-of-red">
+      <div className="flex justify-evenly items-center w-full bg-hint-of-red">
         <img
           src={profile}
           alt="profile"
-          className="h-76 w-76"
+          className="w-76 h-76"
           data-aos="fade-right"
         />
         <div
-          className="my-28 w-1/2 border-8 border-double border-gulf-stream p-20 shadow-2xl shadow-gulf-stream"
+          className="w-1/2 my-28 border-double border-8 border-yellow-border p-20 shadow-2xl shadow-yellow-border"
           data-aos="zoom-in"
         >
-          <p className="font-quicksand-light text-3xl font-semibold text-blue-whale">
+          <p className="font-quicksand-light text-3xl text-blue-whale font-semibold">
             En nuestra plataforma de trabajo, podrás crear un perfil detallado
             que destaque tus habilidades, experiencia y logros. Podrás buscar y
             postularte a ofertas de trabajo que se ajusten a tus habilidades y
@@ -60,10 +60,10 @@ export default function InfoProfessionals() {
           </p>
         </div>
       </div>
-      <div className="flex w-full items-center justify-evenly">
-        <div className="my-28 flex w-1/2 flex-col items-center justify-evenly">
+      <div className="flex justify-evenly items-center w-full">
+        <div className="w-1/2 my-28 flex flex-col items-center justify-evenly">
           <h1
-            className="text-center font-amenable text-5xl font-bold text-blue-whale"
+            className="font-amenable text-5xl text-center text-blue-whale font-bold"
             data-aos="fade-down"
           >
             Éxito laboral
@@ -79,24 +79,24 @@ export default function InfoProfessionals() {
           >
             Si deseas ampliar aún más tus posibilidades de éxito laboral,
             también ofrecemos una suscripción premium que te brindará acceso a{' '}
-            <span className="font-bold underline">contenido exclusivo</span>,
+            <span className="underline font-bold">contenido exclusivo</span>,
             como podcasts con invitados especiales, webinars y videos
             educativos. Estos recursos te ayudarán a mejorar tus habilidades y
             conocimientos laborales, lo que te permitirá destacar en tu búsqueda
             de trabajo.
           </p>
           <Link to="/pricing">
-            <div className="mt-10 rounded-lg bg-gulf-stream px-6 py-4 font-amenable text-xl text-white hover:scale-110 hover:bg-lochinvar">
+            <div className="rounded-lg text-xl bg-gulf-stream px-6 py-4 text-white font-amenable mt-10 hover:scale-110 hover:bg-lochinvar">
               Acceso premium
             </div>
           </Link>
         </div>
-        <img src={job} alt="job" className="h-60 w-60" data-aos="fade-left" />
+        <img src={job} alt="job" className="w-60 h-60" data-aos="fade-left" />
       </div>
-      <div className="flex w-full items-center justify-evenly bg-blue-whale">
-        <div className="my-28 flex w-2/5 flex-col items-center justify-evenly">
+      <div className="flex justify-evenly items-center w-full bg-blue-whale">
+        <div className="w-2/5 my-28 flex flex-col items-center justify-evenly">
           <h1
-            className="text-center font-amenable text-5xl font-bold text-hint-of-red"
+            className="font-amenable text-5xl text-center text-hint-of-red font-bold"
             data-aos="flip-down"
           >
             ¡Únete ahora!
@@ -116,7 +116,7 @@ export default function InfoProfessionals() {
           </p>
           <br />
           <Link to="/auth/register">
-            <div className="border-4 border-gulf-stream px-6 py-2 font-amenable text-2xl font-semibold text-hint-of-red hover:bg-gulf-stream">
+            <div className="px-6 py-2 text-hint-of-red border-4 border-gulf-stream font-amenable text-2xl font-semibold hover:bg-gulf-stream">
               Unirse
             </div>
           </Link>
