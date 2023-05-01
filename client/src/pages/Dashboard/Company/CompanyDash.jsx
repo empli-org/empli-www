f946f6e873f63203342ca04e6172ad3b07f2e725
roblesdotdev <httpsnip@gmail.com>
2023-05-01 15:44:00 -0300

    admin dash index cards

--- client/src/pages/Dashboard/Company/CompanyDash.jsx
+++ client/src/pages/Dashboard/Company/CompanyDash.jsx
@@ -1,7 +1,24 @@
+import { useAccountContext } from '@/pages/Account/AccountContext'
+import DashCards from '../MinCards/DashCards_v2'
+
 export const CompanyDash = () => {
+  const { account } = useAccountContext()
   return (
-    <div>
-      <h1 className="text-xl font-bold">Empresa dash</h1>
+    <div className="flex items-center gap-4 p-4">
+      <pre>{JSON.stringify(account, null, 2)}</pre>
+      <DashCards
+        title="Ofertas publicadas"
+        number={25}
+        label="En la última semana"
+        color="text-gulf-stream"
+        subtitle="Postulaciones: 40"
+      />
+      <DashCards
+        title="Contrataciones"
+        number={2}
+        label="En la última semana"
+        color="text-gray-400"
+      />
     </div>
   )
 }
