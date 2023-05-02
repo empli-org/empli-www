3f217d5cc609c19566eb6e0d22de8d6a9fa485eb
Aldo R. Robles <90198035+roblesdotdev@users.noreply.github.com>
2023-05-02 12:39:18 -0300

    add container to detail news (#120)

--- client/src/pages/Info/NewsDetail.jsx
+++ client/src/pages/Info/NewsDetail.jsx
@@ -1,4 +1,4 @@
-import { DetailPublication } from '@/components'
+import { DetailPublication, Container } from '@/components'
 import { useGetPostByIdQuery } from '@/redux/features/api/base'
 import { useParams } from 'react-router-dom'
 import { motion } from 'framer-motion'
@@ -8,7 +8,7 @@ export default function NewsDetailPage() {
   const { id } = useParams()
   const { data: publication, isLoading } = useGetPostByIdQuery(id)
   return (
-    <div>
+    <Container>
       {isLoading ? (
         <div className="flex h-screen w-full items-center justify-center">
           <motion.button className="p-30 font-2xl m-2 flex transform justify-between gap-x-4 rounded-bl-2xl rounded-tr-lg bg-gray-100 py-2 text-gray-700 shadow-lg transition duration-500 ease-in-out hover:bg-gray-50">
@@ -19,6 +19,6 @@ export default function NewsDetailPage() {
       ) : (
         <DetailPublication publicacion={publication} />
       )}
-    </div>
+    </Container>
   )
 }
