f539f1d34166d9a35d628daba9919421c50e9b32
Aldo R. Robles <90198035+roblesdotdev@users.noreply.github.com>
2023-05-04 19:54:53 -0300

    protected: routes by role (#143)

--- client/src/pages/Dashboard/Professional/ProfessionalDash.jsx
+++ client/src/pages/Dashboard/Professional/ProfessionalDash.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import DashCards from '../MinCards/DashCards_v2'
+import Protected from '../Protected'
 
 export const ProfessionalDash = () => {
   return (
-    <div className="flex items-center gap-4 p-4">
-      <DashCards
-        title="Visitas perfil"
-        number={25}
-        label="En la última semana"
-        color="text-gulf-stream"
-      />
-      <DashCards
-        title="Postulaciones"
-        number={2}
-        label="En la última semana"
-        color="text-gray-400"
-        subtitle="Respondidas: 0"
-      />
-    </div>
+    <Protected accountType="professional">
+      <div className="flex items-center gap-4 p-4">
+        <DashCards
+          title="Visitas perfil"
+          number={25}
+          label="En la última semana"
+          color="text-gulf-stream"
+        />
+        <DashCards
+          title="Postulaciones"
+          number={2}
+          label="En la última semana"
+          color="text-gray-400"
+          subtitle="Respondidas: 0"
+        />
+      </div>
+    </Protected>
   )
 }
